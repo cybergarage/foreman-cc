41fc73299b55319ee8e4161e66f8a11043b5f827
Satoshi Konno <skonno@cybergarage.org>
2017-02-17 01:19:04 +0900

    * Added TimeSeries.

---
+++ src/foreman/TimeSeries.cpp
@@ -0,0 +1,23 @@
+/******************************************************************
+ *
+ * Foreman for C++
+ *
+ * Copyright (C) Satoshi Konno 2017
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <foreman/TimeSeries.h>
+
+using namespace Foreman;
+
+////////////////////////////////////////////////
+// TimeSeries
+////////////////////////////////////////////////
+
+TimeSeries::TimeSeries()
+{
+}
+
+TimeSeries::~TimeSeries() {}
