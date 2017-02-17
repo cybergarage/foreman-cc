081692dcac2f2cc296c5e2482a0db0ef3b0a12e0
Satoshi Konno <skonno@cybergarage.org>
2017-02-17 19:59:15 +0900

    * Removed uhttp_shared_ptr.

--- include/foreman/Platform.h
+++ include/foreman/Platform.h
@@ -15,21 +15,4 @@
 #include "config.h"
 #endif
 
-#if !defined(uhttp_shared_ptr)
-
-#if (defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 201103L)) || defined(__APPLE__)
-#define HAVE_STD_SHRAEDPTR 1
-#define uhttp_shared_ptr std::shared_ptr
-#define uhttp_weak_ptr std::weak_ptr
-#endif
-
-#if !defined(HAVE_STD_SHRAEDPTR)
-#include <boost/shared_ptr.hpp>
-#define uhttp_shared_ptr boost::shared_ptr
-#define uhttp_weak_ptr boost::weak_ptr
-#define nullptr NULL
-#endif
-
-#endif
-
 #endif
