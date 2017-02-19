48241532473de667b410811d0b3ae98f95195ac4
Satoshi Konno <skonno@cybergarage.org>
2017-02-19 23:20:35 +0900

    * Updated tests.

--- tests/MemStoreTestController.cpp
+++ tests/MemStoreTestController.cpp
@@ -31,19 +31,27 @@ void MemStoreTestContoller::run(Foreman::MemStore* store)
   BOOST_CHECK_EQUAL(store->getRowCount(), 0);
   BOOST_CHECK_EQUAL(store->getColumnCount(), 0);
 
+  // Initialize metrics
+  
+  Foreman::Metrics metrics;
+  for (size_t n = 0; n < FORMANCC_MEMSTORETESTCONTROLLER_METRICS_COUNT; n++) {
+    std::ostringstream s;
+    s << FORMANCC_MEMSTORETESTCONTROLLER_METRICS_NAME_PREFIX << n;
+    std::shared_ptr<Foreman::Metric> m = std::shared_ptr<Foreman::Metric>(new Foreman::Metric());
+    m->name = s.str();
+    metrics.push_back(m);
+  }
+
   // Initialize memstore
   
   BOOST_CHECK(store->setRetentionInterval(FORMANCC_MEMSTORETESTCONTROLLER_RETENSION_INTERVAL));
   BOOST_CHECK(store->setRetentionPeriod(FORMANCC_MEMSTORETESTCONTROLLER_RETENSION_PERIOD_SEC));
-  BOOST_CHECK_EQUAL(store->getColumnCount(), (FORMANCC_MEMSTORETESTCONTROLLER_RETENSION_PERIOD_SEC / FORMANCC_MEMSTORETESTCONTROLLER_RETENSION_INTERVAL));
+  BOOST_CHECK_EQUAL(store->getColumnCount(), FORMANCC_MEMSTORETESTCONTROLLER_RETENSION_PERIOD_COUNT);
 
-  for (size_t n = 0; n < FORMANCC_MEMSTORETESTCONTROLLER_METRICS_COUNT; n++) {
-    std::ostringstream s;
-    s << FORMANCC_MEMSTORETESTCONTROLLER_METRICS_NAME_PREFIX << n;
-    Foreman::Metric m;
-    m.name = s.str();
-    store->addMetric(m);
+  for (std::shared_ptr<Foreman::Metric> m : metrics) {
+    store->addMetric(*m);
   }
+
   BOOST_CHECK_EQUAL(store->getRowCount(), FORMANCC_MEMSTORETESTCONTROLLER_METRICS_COUNT);
   BOOST_CHECK(store->realloc());
 
