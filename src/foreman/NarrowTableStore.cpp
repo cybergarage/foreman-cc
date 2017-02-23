a7de1e9e54fbb381e7b526df477d8df30c12e81a
Satoshi Konno <skonno@cybergarage.org>
2017-02-23 22:45:50 +0900

    * Updated NarrowTableStore::getValues().

--- src/foreman/NarrowTableStore.cpp
+++ src/foreman/NarrowTableStore.cpp
@@ -100,7 +100,7 @@ bool NarrowTableStore::addMetric(std::shared_ptr<Metric> m)
   sm->name = m->name;
   sm->rowId = rowId;
 
-  return addMetric(sm);
+  return MemStore::addMetric(sm);
 }
 
 ////////////////////////////////////////////////
@@ -109,17 +109,26 @@ bool NarrowTableStore::addMetric(std::shared_ptr<Metric> m)
 
 bool NarrowTableStore::addValue(const Metric& m)
 {
-  sqlite3_stmt* stmt = NULL;
-
-  int rowId = 0;
-
+  std::shared_ptr<Metric> mm = findMetric(m.name);
+  if (!mm)
+    return false;
+  
+  SQLiteMetric *sm = dynamic_cast<SQLiteMetric*>(mm.get());
+  if (!sm)
+    return false;
+  
+  int rowId = sm->rowId;
+  
   // Insert a value
 
+  sqlite3_stmt* stmt = NULL;
+  
   if (!prepare(FOREMANCC_SQLITESOTORE_RECORD_INSERT, &stmt))
     return false;
   sqlite3_bind_int(stmt, 1, rowId);
   sqlite3_bind_double(stmt, 2, m.value);
   sqlite3_bind_int(stmt, 3, (int)m.timestamp);
+  
   if (sqlite3_step(stmt) != SQLITE_DONE) {
     sqlite3_finalize(stmt);
 
@@ -155,7 +164,26 @@ bool NarrowTableStore::getValues(const Metric& m, time_t beginTs, time_t endTs, 
 
   MetricValue* copyValues = new MetricValue[valueCnt];
 
+  // Select values
+  
+  sqlite3_stmt* stmt = NULL;
+  
+  if (!prepare(FOREMANCC_SQLITESOTORE_RECORD_SELECT_BY_FACTOR_BETWEEN_TIMESTAMP, &stmt))
+    return false;
+  sqlite3_bind_text(stmt, 1, m.name.c_str(), (int)m.name.length(), SQLITE_TRANSIENT);
+  sqlite3_bind_int(stmt, 2, (int)beginTs);
+  sqlite3_bind_int(stmt, 3, (int)endTs);
+  while (sqlite3_step(stmt) == SQLITE_ROW) {
+    double value = sqlite3_column_double(stmt, 1);
+    time_t valueTs = sqlite3_column_int(stmt, 2);
+    ssize_t valueIdx = (valueTs - beginTs) / interval;
+    if (valueIdx < 0 || valueCnt <= valueIdx)
+      continue;
+    copyValues[valueIdx] = value;
+  }
+  sqlite3_finalize(stmt);
+  
   values = std::shared_ptr<MetricValue>(copyValues);
 
-  return false;
+  return true;
 }
