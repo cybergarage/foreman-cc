7b8c7ef934e9bf8ed6dd17c10b62cc1127f17774
Satoshi Konno <skonno@cybergarage.org>
2017-02-25 22:21:22 +0900

    * Added Foreman.h.

---
+++ include/foreman/Foreman.h
@@ -0,0 +1,20 @@
+/******************************************************************
+ *
+ * Foreman for C++
+ *
+ * Copyright (C) Satoshi Konno 2017
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#ifndef _FOREMANCC_FOREMAN_H_
+#define _FOREMANCC_FOREMAN_H_
+
+#include <foreman/Platform.h>
+#include <foreman/Const.h>
+#include <foreman/Metric.h>
+#include <foreman/TimeSeries.h>
+#include <foreman/MemStore.h>
+
+#endif
