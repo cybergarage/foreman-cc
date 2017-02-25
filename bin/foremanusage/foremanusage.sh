8d921bc59e7c3ac5327a623f21224711b93d7ca7
Satoshi Konno <skonno@cybergarage.org>
2017-02-26 02:52:39 +0900

    * Updated foremanbench.

---
+++ bin/foremanusage/foremanusage.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+foremanusage=unix/foremanusage
+
+${foremanusage} matrix 1
+${foremanusage} matrix 4
+${foremanusage} matrix 8
+${foremanusage} matrix 12
+${foremanusage} matrix 24
+
+${foremanusage} ringmap 1
+${foremanusage} ringmap 4
+${foremanusage} ringmap 8
+${foremanusage} ringmap 12
+${foremanusage} ringmap 24
+
+${foremanusage} narrowtable 1
+${foremanusage} narrowtable 4
+${foremanusage} narrowtable 8
+${foremanusage} narrowtable 12
+${foremanusage} narrowtable 24
+
+${foremanusage} tsmap 1
+${foremanusage} tsmap 4
+${foremanusage} tsmap 8
+${foremanusage} tsmap 12
+${foremanusage} tsmap 24
