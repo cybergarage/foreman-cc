9ba9eb4519bd90cc6c80b2a4616afc0466e2a571
Satoshi Konno <skonno@cybergarage.org>
2017-05-03 18:18:09 +0900

    * Updated to ouput the specified record type.

--- bin/foremanusage/foremanusage.sh
+++ bin/foremanusage/foremanusage.sh
@@ -2,6 +2,7 @@
 foremanusage=unix/foremanusage
 
 memStores=(matrix ringmap narrowtable tsmap)
+recordTypes=(random periodic sporadic constant)
 retentionPeriods=(1 4 8 16 32)
 repetationCnt=1
 
@@ -9,12 +10,14 @@ if [ $# -ge 1 ]; then
   repetationCnt=$1
 fi
 
-for m in ${memStores[@]}; do
-  for p in ${retentionPeriods[@]}; do
-    n=$repetationCnt
-    while [ $n -gt 0 ]; do
-      ${foremanusage} $m $p
-      n=$((n-1))
+for r in ${recordTypes[@]}; do
+  for m in ${memStores[@]}; do
+    for p in ${retentionPeriods[@]}; do
+      n=$repetationCnt
+      while [ $n -gt 0 ]; do
+        ${foremanusage} $m $r $p
+        n=$((n-1))
+      done
     done
   done
 done
