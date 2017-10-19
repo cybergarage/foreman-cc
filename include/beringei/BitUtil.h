105dfca501cf9c950ae59cd4ce7f164f3414e238
Satoshi Konno <skonno@cybergarage.org>
2017-10-19 16:03:51 +0900

    * Disabled beringei_setup in boostrap to add the beringei source code directly.

--- include/beringei/BitUtil.h
+++ include/beringei/BitUtil.h
@@ -15,42 +15,42 @@
 namespace facebook {
 namespace gorilla {
 
-class BitUtil {
- public:
-  // Adds a value to a bit string. `bitsInValue` specifies the number
-  // of least significant bits that will be added to the bit
-  // string. The bits from `value` will be added from the most
-  // significant bit to the least significant bit.
-  static void addValueToBitString(
-      uint64_t value,
-      uint64_t bitsInValue,
-      folly::fbstring& bitString,
-      uint32_t& numBits);
+  class BitUtil {
+public:
+    // Adds a value to a bit string. `bitsInValue` specifies the number
+    // of least significant bits that will be added to the bit
+    // string. The bits from `value` will be added from the most
+    // significant bit to the least significant bit.
+    static void addValueToBitString(
+        uint64_t value,
+        uint64_t bitsInValue,
+        folly::fbstring& bitString,
+        uint32_t& numBits);
 
-  // Same as above except this function will throw if too many bits
-  // are attempted to read.
-  // Reads a value from a bit string. `bitPos` is updated by
-  // `bitsToRead`. `bitsToRead` must be 64 or less.
-  static uint64_t readValueFromBitString(
-      folly::StringPiece data,
-      uint64_t& bitPos,
-      uint32_t bitsToRead);
+    // Same as above except this function will throw if too many bits
+    // are attempted to read.
+    // Reads a value from a bit string. `bitPos` is updated by
+    // `bitsToRead`. `bitsToRead` must be 64 or less.
+    static uint64_t readValueFromBitString(
+        folly::StringPiece data,
+        uint64_t& bitPos,
+        uint32_t bitsToRead);
 
-  // Finds the first zero bit and returns its distance from bitPos. If
-  // not found within limit, returns limit.
-  static uint32_t findTheFirstZeroBit(
-      folly::StringPiece data,
-      uint64_t& bitPos,
-      uint32_t limit);
+    // Finds the first zero bit and returns its distance from bitPos. If
+    // not found within limit, returns limit.
+    static uint32_t findTheFirstZeroBit(
+        folly::StringPiece data,
+        uint64_t& bitPos,
+        uint32_t limit);
 
-  // Reads a value until the first zero bit is found or limit reached.
-  // The zero is included in the value as the least significant bit.
-  // `limit` must be 32 or less. Throws an exception if too many bits
-  // are being read.
-  static uint32_t readValueThroughFirstZero(
-      folly::StringPiece data,
-      uint64_t& bitPos,
-      uint32_t limit);
-};
+    // Reads a value until the first zero bit is found or limit reached.
+    // The zero is included in the value as the least significant bit.
+    // `limit` must be 32 or less. Throws an exception if too many bits
+    // are being read.
+    static uint32_t readValueThroughFirstZero(
+        folly::StringPiece data,
+        uint64_t& bitPos,
+        uint32_t limit);
+  };
 }
 } // facebook::gorilla
