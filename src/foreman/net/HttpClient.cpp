611e9d8f2bdc4e38047626462f24b22a317d30d8
Satoshi Konno <skonno@cybergarage.org>
2017-10-26 19:40:11 +0900

    swap uhttp with curl

--- src/foreman/net/HttpClient.cpp
+++ src/foreman/net/HttpClient.cpp
@@ -8,11 +8,8 @@
  *
  ******************************************************************/
 
+#include <curl/curl.h>
 #include <foreman/net/HttpClient.h>
-
-#if defined(FOREMAN_ENABLE_UHTTPCC)
-#include <uhttp/HTTP.h>
-#endif
 
 using namespace Foreman;
 
@@ -20,31 +17,35 @@ using namespace Foreman;
 //  HttpClient
 ////////////////////////////////////////////////
 
-HttpClient::HttpClient()
+/* Curl gets the response in chunks, so we need to collect them into a single
+ * string.
+ */
+size_t HttpClient::curl_response_reader(void* ptr, size_t size, size_t nmemb, std::string* s)
 {
+  size_t previous = s->size();
+  s->append((char*)ptr, size * nmemb);
+  return s->size() - previous;
 }
-
-HttpClient::~HttpClient()
-{
-}
-
-////////////////////////////////////////////////
-//  HttpClient
-////////////////////////////////////////////////
 
 bool HttpClient::get(const std::string& uri, std::string& content)
 {
-#if defined(FOREMAN_ENABLE_UHTTPCC)
-  uHTTP::HTTPRequest httpReq;
-  httpReq.setMethod(uHTTP::HTTP::GET);
-  httpReq.setURL(uri);
+  CURL* curl;
+  CURLcode res;
 
-  uHTTP::HTTPResponse* httpRes = httpReq.post();
-  if (!httpRes->isSuccessful())
-    return false;
+  curl_global_init(CURL_GLOBAL_ALL);
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, uri.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, HttpClient::curl_response_reader);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&content);
 
-  content = httpRes->getContent();
-#endif
-
+    res = curl_easy_perform(curl);
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+    if (res == CURLE_OK)
+      return true;
+  }
   return false;
 }
+
+// vim: ts=2 sw=2 tw=80
