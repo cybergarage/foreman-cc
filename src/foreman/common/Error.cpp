0d5830a42adf8fe9b714907b5a3c01f7c28f0b25
Satoshi Konno <skonno@cybergarage.org>
2017-11-02 15:00:22 +0900

    * Added Error class.

---
+++ src/foreman/common/Error.cpp
@@ -0,0 +1,67 @@
+/******************************************************************
+ *
+ * Foreman for C++
+ *
+ * Copyright (C) 2017 Satoshi Konno
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <foreman/common/Error.h>
+
+Foreman::Error::Error()
+{
+  initialize();
+  this->code = 0;
+}
+
+Foreman::Error::Error(int code)
+{
+  initialize();
+  this->code = code;
+}
+
+Foreman::Error::Error(int code, const std::string& message)
+{
+  initialize();
+  this->code = code;
+  this->message = message;
+}
+
+Foreman::Error::Error(int code, const std::string& message, int detailCode, const std::string& detailMessage)
+{
+  initialize();
+  this->code = code;
+  this->message = message;
+  this->detailCode = detailCode;
+  this->detailMessage = detailMessage;
+}
+
+void Foreman::Error::initialize()
+{
+  this->code = 0;
+  this->detailCode = 0;
+}
+
+Foreman::Error::~Error()
+{
+}
+
+bool Foreman::Error::isError()
+{
+  if (0 < this->code)
+    return true;
+  return false;
+}
+
+bool Foreman::Error::equals(const Error& other) const
+{
+  if (this->getCode() != other.getCode())
+    return false;
+
+  if (this->message.compare(other.getMessage()) != 0)
+    return false;
+
+  return true;
+}
---
+++ test/clang/ClangErrorTests.cpp
@@ -0,0 +1,52 @@
+/******************************************************************
+ *
+ * Foreman for C++
+ *
+ * Copyright (C) 2017 Satoshi Konno
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <boost/test/unit_test.hpp>
+#include <math.h>
+
+#include <foreman/foreman-c.h>
+
+BOOST_AUTO_TEST_SUITE(clang)
+
+BOOST_AUTO_TEST_CASE(ErroDetailSetterTest)
+{
+  ForemanError* err;
+  char msg[32], dmsg[32];
+  int code, dcode;
+  const char *rmsg, *rdmsg;
+  int rcode, rdcode;
+
+  err = foreman_error_new();
+  BOOST_CHECK(err);
+
+  snprintf(msg, sizeof(msg), "msg%d", rand());
+  code = rand();
+  snprintf(dmsg, sizeof(dmsg), "dmsg%d", rand());
+  dcode = rand();
+
+  BOOST_CHECK(foreman_error_setmessage(err, msg));
+  BOOST_CHECK(foreman_error_setcode(err, code));
+  BOOST_CHECK(foreman_error_setdetailmessage(err, dmsg));
+  BOOST_CHECK(foreman_error_setdetailcode(err, dcode));
+
+  BOOST_CHECK(foreman_error_getmessage(err, &rmsg));
+  BOOST_CHECK(foreman_error_getcode(err, &rcode));
+  BOOST_CHECK(foreman_error_getdetailmessage(err, &rdmsg));
+  BOOST_CHECK(foreman_error_getdetailcode(err, &rdcode));
+
+  BOOST_CHECK_EQUAL(msg, rmsg);
+  BOOST_CHECK_EQUAL(code, rcode);
+  BOOST_CHECK_EQUAL(dmsg, rdmsg);
+  BOOST_CHECK_EQUAL(dcode, rdcode);
+
+  BOOST_CHECK(foreman_error_delete(err));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
