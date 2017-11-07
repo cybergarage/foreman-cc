d92ddca11b85084baa6ec08de29846fae1781bba
Satoshi Konno <skonno@cybergarage.org>
2017-11-07 18:42:46 +0900

    * Updated copyright headerss.

--- test/clang/common/ClangErrorTest.cpp
+++ test/clang/common/ClangErrorTest.cpp
@@ -2,7 +2,7 @@
  *
  * Foreman for C++
  *
- * Copyright (C) 2017 Satoshi Konno
+ * Copyright (C) 2017 Satoshi Konno. All rights reserved.
  *
  * This is licensed under BSD-style license, see file COPYING.
  *
