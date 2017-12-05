232b701fb15b0a92336df637f38d97ecfb9a15a6
Satoshi Konno <skonno@cybergarage.org>
2017-12-05 15:08:25 +0900

    * Updated tests for ScriptManager.

---
+++ test/action/ScriptManagerTestController.h
@@ -0,0 +1,44 @@
+/******************************************************************
+ *
+ * Foreman for C++
+ *
+ * Copyright (C) 2017 Satoshi Konno. All rights reserved.
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#ifndef _FOREMANCC_SCRIPT_MANAGERTESTCONTROLLER_H_
+#define _FOREMANCC_SCRIPT_MANAGERTESTCONTROLLER_H_
+
+#include <foreman/action/Script.h>
+
+////////////////////////////////////////////////
+// Constants
+////////////////////////////////////////////////
+
+#define FOREMANCC_SCRIPT_HELLO_ECHO_METHOD "echo"
+#define FOREMANCC_SCRIPT_HELLO_ECHO_PARAM_NAME "message"
+#define FOREMANCC_SCRIPT_HELLO_ECHO_PARAM_VALUE "hello"
+
+////////////////////////////////////////////////
+// ScriptManagerTestController
+////////////////////////////////////////////////
+
+namespace Foreman {
+namespace Action {
+
+  class ScriptManagerTestController {
+public:
+    ScriptManagerTestController();
+    virtual ~ScriptManagerTestController();
+
+    void run(ScriptManager* scriptMgr);
+
+private:
+    void testHello(ScriptManager* scriptMgr);
+  };
+}
+}
+
+#endif
