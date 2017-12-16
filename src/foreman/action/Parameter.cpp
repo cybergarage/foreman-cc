d2f9ca404f94d4bab4070eff1bb0d7c28e1ae76b
Satoshi Konno <skonno@cybergarage.org>
2017-12-16 18:44:52 +0900

    Rearrange class to use decltype on member

---
+++ src/foreman/action/Parameter.cpp
@@ -0,0 +1,27 @@
+/******************************************************************
+ *
+ * Foreman for C++
+ *
+ * Copyright (C) 2017 Satoshi Konno. All rights reserved.
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <foreman/action/Parameter.h>
+
+using namespace Foreman::Action;
+
+////////////////////////////////////////////////
+// Parameter
+////////////////////////////////////////////////
+
+Parameter::Parameter()
+{
+  setType(UnknownType);
+}
+
+Parameter::Parameter(ParameterType type)
+{
+  setType(type);
+}
