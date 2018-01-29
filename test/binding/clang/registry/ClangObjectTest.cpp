8d4a820621247ab94f26178af39f071a795622c4
Satoshi Konno <skonno@cybergarage.org>
2018-01-30 03:05:51 +0900

    * Fixed duplicate symbols.

--- test/binding/clang/registry/ClangObjectTest.cpp
+++ test/binding/clang/registry/ClangObjectTest.cpp
@@ -14,14 +14,14 @@
 
 BOOST_AUTO_TEST_SUITE(clang)
 
-BOOST_AUTO_TEST_CASE(NewObject)
+BOOST_AUTO_TEST_CASE(NewRegistryObject)
 {
   ForemanRegistryObject* obj = foreman_registry_object_new();
   BOOST_CHECK(obj);
   BOOST_CHECK(foreman_registry_object_delete(obj));
 }
 
-BOOST_AUTO_TEST_CASE(NewObjects)
+BOOST_AUTO_TEST_CASE(NewRegistryObjects)
 {
   ForemanRegistryObjects* objs = foreman_registry_objects_new();
   BOOST_CHECK(objs);
