8f284c516007d7af622f5a189cdb806d39bf8779
Satoshi Konno <skonno@cybergarage.org>
2018-01-30 03:43:27 +0900

    Updated tests for the register functions.

--- test/register/StoreTestController.cpp
+++ test/register/StoreTestController.cpp
@@ -32,5 +32,28 @@ void StoreTestContoller::run(Store* store)
 {
   BOOST_CHECK(store->open());
 
+  Foreman::Error err;
+  
+  for (int n=0; n<10; n++) {
+    std::stringstream ss;
+    time_t ts;
+    
+    Object inObj;
+    time(&ts); ts += rand(); ss << ts;
+    std::string key = "key" + ss.str();
+    inObj.setKey(key);
+    std::string val = "val" + ss.str();
+    inObj.setData(val);
+    
+    BOOST_CHECK_EQUAL(store->size(), n);
+    BOOST_CHECK_EQUAL(store->setObject(&inObj, &err), true);
+    BOOST_CHECK_EQUAL(store->size(), (n+1));
+    
+    Object outObj;
+    
+    BOOST_CHECK_EQUAL(store->getObject(key, &outObj, &err), true);
+    BOOST_CHECK_EQUAL(inObj.equals(&outObj), true);
+  }
+
   BOOST_CHECK(store->close());
 }
