30918098451bcb34226b49d820198c60f893de77
Satoshi Konno <skonno@cybergarage.org>
2018-01-30 21:49:03 +0900

    Added SocketImp.h.

---
+++ include/foreman/register/impl/StoreImpl.h
@@ -0,0 +1,16 @@
+/******************************************************************
+ *
+ * Foreman for C++
+ *
+ * Copyright (C) 2017 Satoshi Konno. All rights reserved.
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#ifndef _FOREMANCC_REGISTER_STOREIMPL_H_
+#define _FOREMANCC_REGISTER_STOREIMPL_H_
+
+#include <foreman/register/impl/MemStore.h>
+
+#endif
