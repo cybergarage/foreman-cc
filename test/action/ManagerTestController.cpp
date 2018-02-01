ef6e9449dba06f113eada65b96c51b861dea5f6a
Satoshi Konno <skonno@cybergarage.org>
2018-02-01 13:47:16 +0900

    Updated test case for Python.

--- test/action/ManagerTestController.cpp
+++ test/action/ManagerTestController.cpp
@@ -78,15 +78,56 @@ void ManagerTestController::testHello(Manager* mgr)
 void ManagerTestController::testRegister(Manager* mgr)
 {
   Parameters params;
-  auto param = new String();
+  Parameters results;
+  String* param;
+  Error err;
+  bool isSuccess;
+
+  // Set registry
+
+  params.clear();
+  param = new String();
   param->setName(FOREMANCC_SCRIPT_SET_REGISTER_METHOD_PARAM_NAME);
   param->setValue(FOREMANCC_SCRIPT_SET_REGISTER_METHOD_PARAM_VALUE);
   params.addParameter(param);
 
-  Parameters results;
-  Error err;
-  auto isExecuted = mgr->execMethod(FOREMANCC_SCRIPT_SET_REGISTER_METHOD, &params, &results, &err);
-  BOOST_CHECK(isExecuted);
-  if (!isExecuted)
+  results.clear();
+  isSuccess = mgr->execMethod(FOREMANCC_SCRIPT_SET_REGISTER_METHOD, &params, &results, &err);
+  BOOST_CHECK(isSuccess);
+  if (!isSuccess)
     return;
+
+  // Get registry
+
+  params.clear();
+  param = new String();
+  param->setName(FOREMANCC_SCRIPT_SET_REGISTER_METHOD_PARAM_NAME);
+  params.addParameter(param);
+
+  results.clear();
+  isSuccess = mgr->execMethod(FOREMANCC_SCRIPT_GET_REGISTER_METHOD, &params, &results, &err);
+  BOOST_CHECK(isSuccess);
+  if (!isSuccess)
+    return;
+
+  auto retParam = results.getParameter(FOREMANCC_SCRIPT_SET_REGISTER_METHOD_PARAM_NAME);
+  BOOST_CHECK(retParam);
+  BOOST_CHECK(retParam->isString());
+  auto retString = dynamic_cast<String*>(retParam);
+  BOOST_CHECK(retString);
+  BOOST_CHECK_EQUAL(retString->getValue().c_str(), FOREMANCC_SCRIPT_SET_REGISTER_METHOD_PARAM_VALUE);
+
+  // Remove registry
+
+  // FIXME : Python error
+  /*
+  params.clear();
+  param = new String();
+  param->setName(FOREMANCC_SCRIPT_SET_REGISTER_METHOD_PARAM_NAME);
+  params.addParameter(param);
+  
+  results.clear();
+  isSuccess = mgr->execMethod(FOREMANCC_SCRIPT_REMOVE_REGISTER_METHOD, &params, &results, &err);
+  BOOST_CHECK(isSuccess);
+   */
 }
