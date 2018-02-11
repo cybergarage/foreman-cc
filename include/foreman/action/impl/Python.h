ff8866d5947fc9e9a7ac1e4acfb466e83264a813
Satoshi Konno <skonno@cybergarage.org>
2018-02-11 15:29:56 +0900

    Removed an extra defined(FOREMAN_SUPPORT_PYTHON).

--- include/foreman/action/impl/Python.h
+++ include/foreman/action/impl/Python.h
@@ -12,17 +12,14 @@
 #define _FOREMANCC_ACTION_PYTHON_H_
 
 #include <foreman/Platform.h>
-
-#if defined(FOREMAN_SUPPORT_PYTHON)
-
 #include <foreman/action/Engine.h>
 
 #if defined(FOREMAN_SUPPORT_PYTHON)
+
 #if defined(HAVE_PYTHON_PYTHON_H)
 #include <Python/Python.h>
 #else
 #include <Python.h>
-#endif
 #endif
 
 namespace Foreman {
