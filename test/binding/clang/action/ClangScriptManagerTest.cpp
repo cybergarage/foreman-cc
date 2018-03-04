638cca7de97487e6fd1e1d88751ab7acda99109d
Satoshi Konno <skonno@cybergarage.org>
2018-03-04 19:27:35 +0900

    Fixed foreman_action_manager_nextmethod() to return a correct next method.

--- test/binding/clang/action/ClangScriptManagerTest.cpp
+++ test/binding/clang/action/ClangScriptManagerTest.cpp
@@ -21,9 +21,29 @@ BOOST_AUTO_TEST_CASE(NewActionScriptManager)
   mgr = foreman_action_manager_new();
   BOOST_CHECK(mgr);
 
+  auto err = foreman_error_new();
+  
+  // Check first method
+  
   auto method = foreman_action_manager_getfirstmethod(mgr);
   BOOST_CHECK(!method);
+
+  // Add a method
   
+  method = foreman_action_method_new("");
+  BOOST_CHECK(foreman_action_method_setname(method, "test"));
+  BOOST_CHECK(foreman_action_manager_addmethod(mgr, method, err));
+
+  // Check first method
+
+  method = foreman_action_manager_getfirstmethod(mgr);
+  BOOST_CHECK(method);
+  
+  method = foreman_action_manager_nextmethod(mgr, method);
+  BOOST_CHECK(!method);
+  
+  foreman_error_delete(err);
+              
   BOOST_CHECK(foreman_action_manager_delete(mgr));
 }
 
