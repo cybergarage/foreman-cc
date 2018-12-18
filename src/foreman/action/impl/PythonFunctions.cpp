05817343f3e3d584482f5d21eade552c8c708058
Satoshi Konno <skonno@cybergarage.org>
2018-12-18 11:05:14 +0900

    Add foreman_python_executequery() #29

--- src/foreman/action/impl/PythonFunctions.cpp
+++ src/foreman/action/impl/PythonFunctions.cpp
@@ -90,6 +90,66 @@ PyObject* foreman_python_removeregister(PyObject* self, PyObject* args)
 }
 
 /****************************************
+ * foreman_python_executequery
+ ****************************************/
+
+#define foreman_python_parsejson_module FOREMANCC_PRODUCT_NAME "_internal"
+#define foreman_python_parsejson_method "foreman_python_parsejson"
+#define foreman_python_parsejson_code    \
+  "import json\n"                        \
+  "def foreman_python_parsejson(str):\n" \
+  "    return json.loads(str)\n"
+
+PyObject* foreman_python_executequery(PyObject* self, PyObject* args)
+{
+  const char* query;
+  if (!PyArg_ParseTuple(args, "s", &query))
+    return NULL;
+
+  // Comile
+
+  PyObject* pSource = Py_CompileString(foreman_python_parsejson_code, foreman_python_parsejson_method, Py_file_input);
+  if (!pSource) {
+    return NULL;
+  }
+
+  PyObject* pyModule = PyImport_ExecCodeModuleEx((char*)foreman_python_parsejson_module, pSource, (char*)foreman_python_parsejson_method);
+  Py_DECREF(pSource);
+  if (pyModule) {
+    return NULL;
+  }
+
+  PyObject* pyFunc = PyObject_GetAttrString(pyModule, foreman_python_parsejson_method);
+  if (!pyFunc || !PyCallable_Check(pyFunc)) {
+    Py_DECREF(pyModule);
+    return NULL;
+  }
+
+  // Exec method
+
+  PyObject* pArgs = PyTuple_New(2);
+  if (!pArgs) {
+    return NULL;
+  }
+
+  PyTuple_SetItem(pArgs, 0, PyUnicode_FromString(query));
+
+  PyObject* pResults = PyObject_CallObject(pyFunc, pArgs);
+
+  Py_DECREF(pyFunc);
+  Py_DECREF(pyModule);
+  Py_DECREF(pArgs);
+
+  if (!pResults) {
+    return NULL;
+  }
+
+  Py_DECREF(pResults);
+
+  return pResults;
+}
+
+/****************************************
  * Python Modules
  ****************************************/
 
@@ -97,6 +157,7 @@ static PyMethodDef gForemanPythonMethods[] = {
   { FOREMANCC_SYSTEM_FUNCTION_SETREGISTER, foreman_python_setregister, METH_VARARGS, "" },
   { FOREMANCC_SYSTEM_FUNCTION_GETREGISTER, foreman_python_getregister, METH_VARARGS, "" },
   { FOREMANCC_SYSTEM_FUNCTION_REMOVEREGISTER, foreman_python_removeregister, METH_VARARGS, "" },
+  { FOREMANCC_SYSTEM_FUNCTION_EXECUTEQUERY, foreman_python_executequery, METH_VARARGS, "" },
   { NULL, NULL, 0, NULL }
 };
 
