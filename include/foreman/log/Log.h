fb187285be42a0d9ded30c0f58047b6758b016d4
Satoshi Konno <skonno@cybergarage.org>
2019-02-08 10:20:39 +0900

    Fix to compile on MacOSX

--- include/foreman/log/Log.h
+++ include/foreman/log/Log.h
@@ -13,28 +13,16 @@
 
 #include <foreman/log/Logger.h>
 
-#if defined(__USE_ISOC99)
 #if defined(DEBUG)
 #define FOREMAN_LOG_DEBUG(format, ...) Foreman::Log::Logger::GetSharedInstance()->message(Foreman::Log::DEBUG, format, __VA_ARGS__)
 #else
 #define FOREMAN_LOG_DEBUG(format, ...)
 #endif
+
 #define FOREMAN_LOG_TRACE(format, ...) Foreman::Log::Logger::GetSharedInstance()->message(Foreman::Log::TRACE, format, __VA_ARGS__)
 #define FOREMAN_LOG_INFO(format, ...) Foreman::Log::Logger::GetSharedInstance()->message(Foreman::Log::INFO, format, __VA_ARGS__)
 #define FOREMAN_LOG_WARN(format, ...) Foreman::Log::Logger::GetSharedInstance()->message(Foreman::Log::WARN, format, __VA_ARGS__)
 #define FOREMAN_LOG_ERROR(format, ...) Foreman::Log::Logger::GetSharedInstance()->message(Foreman::Log::ERROR, format, __VA_ARGS__)
 #define FOREMAN_LOG_FATAL(format, ...) Foreman::Log::Logger::GetSharedInstance()->message(Foreman::Log::FATAL, format, __VA_ARGS__)
-#else
-#if defined(DEBUG)
-#define FOREMAN_LOG_DEBUG(format...) Foreman::Log::Logger::GetSharedInstance()->message(Foreman::Log::DEBUG, format, __VA_ARGS__)
-#else
-#define FOREMAN_LOG_DEBUG(format...)
-#endif
-#define FOREMAN_LOG_TRACE(format...) Foreman::Log::Logger::GetSharedInstance()->message(Foreman::Log::TRACE, format, __VA_ARGS__)
-#define FOREMAN_LOG_INFO(format...) Foreman::Log::Logger::GetSharedInstance()->message(Foreman::Log::INFO, format, __VA_ARGS__)
-#define FOREMAN_LOG_WARN(format...) Foreman::Log::Logger::GetSharedInstance()->message(Foreman::Log::WARN, format, __VA_ARGS__)
-#define FOREMAN_LOG_ERROR(format...) Foreman::Log::Logger::GetSharedInstance()->message(Foreman::Log::ERROR, format, __VA_ARGS__)
-#define FOREMAN_LOG_FATAL(format...) Foreman::Log::Logger::GetSharedInstance()->message(Foreman::Log::FATAL, format, __VA_ARGS__)
-#endif
 
 #endif
