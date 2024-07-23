d61eae60a9888112e840d91ceaf1e045bf2b651d
Satoshi Konno <skonno@cybergarage.org>
2024-07-23 18:30:19 +0900

    Update ScriptManagerPythonActions

--- test/binding/clang/action/ClangScriptManagerTest.cpp
+++ test/binding/clang/action/ClangScriptManagerTest.cpp
@@ -9,6 +9,8 @@
  ******************************************************************/
 
 #include <boost/test/unit_test.hpp>
+#include <string>
+#include <vector>
 
 #include <foreman/Platform.h>
 #include <foreman/foreman-c.h>
@@ -19,26 +21,29 @@
 
 BOOST_AUTO_TEST_SUITE(clang)
 
-BOOST_AUTO_TEST_CASE(NewActionScriptManager)
+BOOST_AUTO_TEST_CASE(ScriptManagerPythonActions)
 {
   auto TEST_METHOD_LANG = "python";
-  auto TEST_METHOD_NAME = "qos_unsatisfied";
-  auto TEST_METHOD_CODE = "import foreman\n"
-                          "def qos_unsatisfied(params,results):\n"
-                          "    foreman.set_register('qos', 'false')\n"
-                          "    return True";
-
-  auto err = foreman_error_new();
-  auto inParams = foreman_action_parameters_new();
-  auto outParams = foreman_action_parameters_new();
+  std::vector<std::string> TEST_METHOD_NAMES = {
+    "echo",
+    "qos_unsatisfied"
+  };
+  std::vector<std::string> TEST_METHOD_CODES = {
+    "import foreman\n"
+    "def echo(params,results):\n"
+  	"    for key, value in params.items():\n"
+		"        results[key] = value\n"
+	  "    return True",
+    "import foreman\n"
+    "def qos_unsatisfied(params,results):\n"
+    "    foreman.set_register('qos', 'false')\n"
+    "    return True"
+  };
 
   // Setup
 
   auto mgr = foreman_action_manager_new();
   BOOST_CHECK(mgr);
-
-  auto regstryStore = foreman_registry_store_new();
-  BOOST_CHECK(foreman_action_manager_setregistrystore(mgr, regstryStore));
 
   auto regsterStore = foreman_register_store_new();
   BOOST_CHECK(foreman_action_manager_setregisterstore(mgr, regsterStore));
@@ -48,43 +53,36 @@ BOOST_AUTO_TEST_CASE(NewActionScriptManager)
   auto method = foreman_action_manager_getfirstmethod(mgr);
   BOOST_CHECK(!method);
 
-  // Add a method
+  // Add methods
 
-  method = foreman_action_method_new(TEST_METHOD_LANG);
-  BOOST_CHECK(foreman_action_method_setname(method, TEST_METHOD_NAME));
-  BOOST_CHECK(foreman_action_method_setstringcode(method, TEST_METHOD_CODE));
-  BOOST_CHECK(foreman_action_manager_addmethod(mgr, method, err));
-
-  // Check first method
-
-  method = foreman_action_manager_getfirstmethod(mgr);
-  BOOST_CHECK(method);
-  if (!method) {
-    return;
+  for (size_t n = 0; n < TEST_METHOD_NAMES.size(); n++) {
+    auto err = foreman_error_new();
+    method = foreman_action_method_new(TEST_METHOD_LANG);
+    BOOST_CHECK(foreman_action_method_setname(method, TEST_METHOD_NAMES[n].c_str()));
+    BOOST_CHECK(foreman_action_method_setstringcode(method, TEST_METHOD_CODES[n].c_str()));
+    BOOST_CHECK(foreman_action_manager_addmethod(mgr, method, err));
+    foreman_error_delete(err);
   }
 
-  method = foreman_action_manager_nextmethod(mgr, method);
-  BOOST_CHECK(!method);
-
-  // Exec the valid method
-
+  // Exec methods
+  for (size_t n = 0; n < TEST_METHOD_NAMES.size(); n++) {
+      auto inParams = foreman_action_parameters_new();
 #if defined(FOREMAN_SUPPORT_PYTHON)
-  for (auto n = 0; n < 10; n++) {
-    BOOST_CHECK(foreman_action_manager_execmethod(mgr, TEST_METHOD_NAME, inParams, outParams, err));
-  }
+    for (auto n = 0; n < 1; n++) {
+      auto err = foreman_error_new();
+      auto outParams = foreman_action_parameters_new();
+      BOOST_CHECK(foreman_action_manager_execmethod(mgr, TEST_METHOD_NAMES[n].c_str(), inParams, outParams, err));
+      foreman_error_delete(err);
+      BOOST_CHECK(foreman_action_parameters_delete(outParams));
+    }
 #endif
+      BOOST_CHECK(foreman_action_parameters_delete(inParams));
+  }
 
   // Finalize
 
   BOOST_CHECK(foreman_action_manager_delete(mgr));
-
-  BOOST_CHECK(foreman_registry_store_delete(regstryStore));
   BOOST_CHECK(foreman_register_store_delete(regsterStore));
-
-  BOOST_CHECK(foreman_action_parameters_delete(inParams));
-  BOOST_CHECK(foreman_action_parameters_delete(outParams));
-
-  foreman_error_delete(err);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
